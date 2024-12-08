8b28bfddb64e8c4fd5e042a51f31a0c963f0062e
shar0ff <sharovivan17@gmail.com>
2024-12-08 23:24:17 +0100

    Add footer and header partials. Create Assignments model

---
+++ model/assignments_db.php
@@ -0,0 +1,41 @@
+<?php 
+    function getAssignmentsByCourse($courseId){
+        global $db;
+
+        if ($courseId){
+            $query = 'SELECT A.ID, A.Dscription, C.courseName FROM assignments A LEFT JOIN 
+            courses C ON A.courseID = C.courseID WHERE A.courseID = :course_id ORDER BY A.ID';
+        }else{
+            $query = 'SELECT A.ID, A.Dscription, C.courseName FROM assignments A LEFT JOIN 
+            courses C ON A.courseID = C.courseID ORDER BY C.courseID';
+        }
+        $statment = $db->prepare($query);
+        $statment->bindValue(':course_id', $courseId);
+        $statment->execute();
+        $assignments = $statment->fetchAll();
+        $statment->closeCursor();
+        return $statment;
+    }
+
+    function delete_assignment($assignmentId){
+        global $db;
+        
+        $query = 'DELETE FROM assignments WHERE ID = :assignment_id';
+        $statment = $db->prepare($query);
+        $statment->bindValue(':assignment_id', $assignmentId);
+        $statment->execute();
+        $statment->closeCursor();
+    }
+
+    function add_assignment($courseId, $description){
+        global $db;
+
+        $query = 'INSERT INTO assignments (Description, courseID) VALUES (:description, :courseID)';
+
+        $statment = $db->prepare($query);
+        $statment->bindValue(':description', $description);
+        $statment->bindValue(':courseID', $courseID);
+        $statment->execute();
+        $statment->closeCursor();
+    }
+?>
